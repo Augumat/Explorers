a776101d78e1a77cab092930886d6c35c77a8871
riya jain <rjain35@illinois.edu>
2018-12-11 23:02:10 -0600

    rest of the questions work

--- app/src/main/java/com/waow/rmm/explorers/Question9.java
+++ app/src/main/java/com/waow/rmm/explorers/Question9.java
@@ -1,13 +1,54 @@
 package com.waow.rmm.explorers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Question9 extends AppCompatActivity {
 
+    final String TAG = "clickedAnswer";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question9);
+        Button answerA = findViewById(R.id.answerA);
+        answerA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "clicked A");
+                openActivityResult();
+            }
+        });
+        Button answerB = findViewById(R.id.answerB);
+        answerB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "clicked B");
+                openActivityResult();
+            }
+        });
+        Button answerC = findViewById(R.id.answerC);
+        answerC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "clicked C");
+                openActivityResult();
+            }
+        });
+        Button answerD = findViewById(R.id.answerD);
+        answerD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "clicked D");
+                openActivityResult();
+            }
+        });
+    }
+    public void openActivityResult() {
+        Intent openResult = new Intent(this, Question10.class);
+        startActivity(openResult);
     }
 }
