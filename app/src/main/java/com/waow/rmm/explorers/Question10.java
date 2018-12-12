7e48eb1364dc084a80920879ca6345242ee97d56
Matthew Augustyn <ma62@illinois.edu>
2018-12-11 21:35:03 -0600

    results

--- app/src/main/java/com/waow/rmm/explorers/Question10.java
+++ app/src/main/java/com/waow/rmm/explorers/Question10.java
@@ -1,13 +1,83 @@
 package com.waow.rmm.explorers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class Question10 extends AppCompatActivity {
-
+public class Question10 extends AppCompatActivity
+{
+    /** The logging tag for this class. */
+    private final static String TAG = "MainActivity";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question10);
+        
+        Button answerA = findViewById(R.id.answerA);
+        answerA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "finish quiz");
+                Quiz.add("a");
+                openActivityResults(Quiz.tally());
+            }
+        });
+    
+        Button answerB = findViewById(R.id.answerB);
+        answerB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "finish quiz");
+                Quiz.add("bb");
+                openActivityResults(Quiz.tally());
+            }
+        });
+    
+        Button answerC = findViewById(R.id.answerC);
+        answerC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "finish quiz");
+                Quiz.add("ccc");
+                openActivityResults(Quiz.tally());
+            }
+        });
+    
+        Button answerD = findViewById(R.id.answerD);
+        answerD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "finish quiz");
+                Quiz.add("dddd");
+                openActivityResults(Quiz.tally());
+            }
+        });
+    }
+    
+    public void openActivityResults(int which) {
+        Intent openResult;
+        switch (which)
+        {
+            case 1:
+                openResult = new Intent(this, ResultsA.class);
+                break;
+            case 2:
+                openResult = new Intent(this, ResultsB.class);
+                break;
+            case 3:
+                openResult = new Intent(this, ResultsC.class);
+                break;
+            case 4:
+                openResult = new Intent(this, ResultsD.class);
+                break;
+            default:
+                openResult = new Intent(this, ResultsA.class);
+                break;
+        }
+        startActivity(openResult);
     }
 }
