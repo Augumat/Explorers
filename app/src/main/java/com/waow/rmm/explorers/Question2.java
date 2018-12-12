d38e0df3a8ee4a5ef64e1016a4945cca09b8010a
riya jain <rjain35@illinois.edu>
2018-12-11 21:11:45 -0600

    new questions

--- app/src/main/java/com/waow/rmm/explorers/Question2.java
+++ app/src/main/java/com/waow/rmm/explorers/Question2.java
@@ -3,11 +3,11 @@ package com.waow.rmm.explorers;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class question1 extends AppCompatActivity {
+public class Question2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question1);
+        setContentView(R.layout.activity_question2);
     }
 }
