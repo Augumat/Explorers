aaf48085f2b5ecc363aed83337e726b1f27dac15
riya jain <rjain35@illinois.edu>
2018-12-11 21:47:20 -0600

    buttons on question 1 work

--- app/src/main/java/com/waow/rmm/explorers/Question1.java
+++ app/src/main/java/com/waow/rmm/explorers/Question1.java
@@ -1,13 +1,54 @@
 package com.waow.rmm.explorers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Question1 extends AppCompatActivity {
 
+    final String TAG = "clickedAnswer";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question1);
+        Button answerA = findViewById(R.id.answerA);
+        answerA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "clicked A");
+                openQuestion2();
+            }
+        });
+        Button answerB = findViewById(R.id.answerB);
+        answerB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "clicked B");
+                openQuestion2();
+            }
+        });
+        Button answerC = findViewById(R.id.answerC);
+        answerC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "clicked C");
+                openQuestion2();
+            }
+        });
+        Button answerD = findViewById(R.id.answerD);
+        answerD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "clicked D");
+                openQuestion2();
+            }
+        });
+    }
+    public void openQuestion2() {
+        Intent openResult = new Intent(this, Question2.class);
+        startActivity(openResult);
     }
 }
