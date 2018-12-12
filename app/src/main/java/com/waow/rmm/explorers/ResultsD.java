0b0fb78b6207c6453deea323711ac0e1d28e7ace
Matthew Augustyn <ma62@illinois.edu>
2018-12-11 21:54:08 -0600

    finished results

--- app/src/main/java/com/waow/rmm/explorers/ResultsD.java
+++ app/src/main/java/com/waow/rmm/explorers/ResultsD.java
@@ -1,6 +1,36 @@
 package com.waow.rmm.explorers;
 
-public class ResultsD
-{
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+public class ResultsD extends AppCompatActivity
+{
+    /** The logging tag for this class. */
+    private final static String TAG = "ResultsD";
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results_d);
+        
+        Button beginQuiz = findViewById(R.id.beginQuiz);
+        beginQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "back to main");
+                openActivityReset();
+            }
+        });
+    }
+    
+    public void openActivityReset() {
+        Intent openQuestion = new Intent(this, MainActivity.class);
+        Quiz.reset();
+        startActivity(openQuestion);
+    }
 }
